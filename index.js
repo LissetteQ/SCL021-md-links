ae50b30d0d1ccce7c294c12bfd38d9c842432940
LissetteQ <lissetteq123@gmail.com>
2022-10-04 11:41:30 -0300

    Incio de la ruta

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = () => {
-  // ...
-};
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require ("path");
+/*function MdLink (ruta) {
+  console.log("hola", ruta);
+}
+MdLink("./readme.md");*/
+
+let inputPath = process.argv [2]
+console.log({inputPath});
+const pathfixed = path.posix.basename(inputPath);
+console.log({pathfixed});
+const routeExist = pathfixed => fs.existsSync(pathfixed);
+console.log(routeExist(pathfixed))
