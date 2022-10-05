915f9cd1daf90241b59d08fe306fb305a20aa7e3
LissetteQ <lissetteq123@gmail.com>
2022-10-05 18:00:10 -0300

    La ruta es relativa

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+//importamos fs y path
 const fs = require('fs');
 const path = require ("path");
 /*function MdLink (ruta) {
@@ -6,9 +7,24 @@ const path = require ("path");
 }
 MdLink("./readme.md");*/
 
+//comprabando si las rutas existen 
 let inputPath = process.argv [2]
 console.log({inputPath});
 const pathfixed = path.posix.basename(inputPath);
 console.log({pathfixed});
 const routeExist = pathfixed => fs.existsSync(pathfixed);
 console.log(routeExist(pathfixed))
+
+//comprobamos si es archivo md
+const routeType = (source) => {
+  if(source.isFile() === true) {
+      return true
+  } return false
+}
+const routeIsAFile = fs.statSync(pathfixed);
+console.log(routeType(routeIsAFile))
+//console.log(routeIsAFile);
+
+//la ruta es relativa
+const file = fs.readFileSync(pathfixed,  {encoding:'utf8', flag:'r'} );
+console.log(file);
