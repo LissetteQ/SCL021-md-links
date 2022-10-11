75219dd5ea82fe6d0f1262c1eca16d25cdca9a88
LissetteQ <lissetteq123@gmail.com>
2022-10-11 17:56:03 -0300

    Se leen archivos MD

--- index.js
+++ index.js
@@ -3,35 +3,54 @@
 const fs = require("fs");
 const path = require("path");
 const { readFileSync } = require("node:fs");
-const { argv } = require('node:process');
-
-
-function MdLink (ruta) {
+const { argv } = require("node:process");
+function MdLink(ruta) {
   //console.log("hola", ruta);
 
-//comprabando si las rutas existen
-const routeExist = () => fs.existsSync(ruta);
-console.log(routeExist());
+  //comprabando si las rutas existen
+  const routeExist = () => fs.existsSync(ruta);
+  console.log(routeExist());
 
-//comprobamos si es archivo 
-const routeType = (source) => {
-  if (source.isFile() === true) {
-    return true;
+  //comprobamos si es archivo
+  const routeType = (source) => {
+    if (source.isFile() === true) {
+      return true;
+    }
+    return false;
+  };
+  //leer archivos de tu ruta principal
+  const routeFiles = fs.statSync(ruta);
+  console.log("es archivo? " + routeType(routeFiles));
+  //console.log(routeIsAFile);
+  //HASTA AQUI OK
+
+  //lee los archivos de la ruta relativa
+  const dir = fs.readdirSync(ruta, { encoding: "utf8", flag: "r" });
+  console.log("estos son los archivos del directorios" + dir);
+  console.log(dir);
+
+  //filtro de archivo md
+  let array = [];
+  function rute(dir) {
+    return (array = dir.filter((archivo) => {
+      return path.extname(archivo) === ".md";
+      //console.log(array);
+    }));
   }
-  return false;
-};
-//leer archivos de tu ruta principal
-const routeFiles = fs.statSync(ruta);
-console.log('es archivo? ' + routeType(routeFiles));
-//console.log(routeIsAFile);
-//HASTA AQUI OK
+  //leer archivos MD
+  const arrayMd = rute(dir);
+  function readMD(paths) {
+    console.log("ejecutando path");
+    paths.forEach((element) => {
+      const data = fs.readFileSync(element, { encoding: "utf8", flag: "r" });
+      console.log("leyendo", data);
+    });
+  }
+  readMD(arrayMd);
+  console.log(rute(dir));
 
-//lee los archivos de la ruta relativa
-const dir = fs.readdirSync(ruta, { encoding: "utf8", flag: "r" });
-
-console.log('estos son los archivos del directorios' + dir);
-
-
-//lee directorios
+  //Existen los links ?
+  
 }
+
 MdLink("./");
